b56c6ac63577313f019561f6c25b45cada3af873
Artur Vasyliev <princeartik@gmail.com>
2020-07-16 11:59:55 +0300

    Added start page logic.

---
+++ AlloyTraining/Models/Pages/StartPage.cs
@@ -0,0 +1,38 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+
+namespace AlloyTraining.Models.Pages
+{
+    [ContentType(
+        DisplayName = "Start", 
+        GUID = "a293d16f-7e7d-4f6a-8d69-47985f9e7442", 
+        Description = "The home page for a website with an area for blocks and partial pages.",
+        GroupName = SiteGroupNames.Specialized,
+        Order = 10)]
+    public class StartPage : PageData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Heading", 
+            Description = "If the Heading is not set, the page falls back to showing the Name.",
+            GroupName = SystemTabNames.Content, 
+            Order = 10)]
+        public virtual string Heading { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 20)]
+        public virtual XhtmlString MainBody { get; set; }
+
+        [Display(Name = "Main content area",
+            Description = "The main content area contains an ordered collection to content references, for example blocks, media assets, and pages.",
+            GroupName = SystemTabNames.Content,
+            Order = 30)]
+        public virtual ContentArea MainContentArea { get; set; }
+    }
+}

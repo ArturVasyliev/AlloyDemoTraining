3ca6efbd28ab079a5430fef7ab9176f3a734eff9
Artur Vasyliev <princeartik@gmail.com>
2020-07-18 20:57:31 +0300

    Created a partial template for all pages

--- AlloyTraining/Controllers/ContentFolderController.cs
+++ AlloyTraining/Controllers/ContentFolderController.cs
@@ -6,11 +6,13 @@ using System.Web.Mvc;
 using AlloyTraining.Models.ViewModels;
 using EPiServer;
 using EPiServer.Core;
+using EPiServer.Framework.DataAnnotations;
 using EPiServer.Web;
 using EPiServer.Web.Mvc;
 
 namespace AlloyTraining.Controllers
 {
+    [TemplateDescriptor]
     public class ContentFolderController : PartialContentController<ContentFolder>
     {
         private readonly IContentLoader loader;

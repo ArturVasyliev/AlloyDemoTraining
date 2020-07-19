27f272912a506914159a56edeaec747bd2529e18
Artur Vasyliev <princeartik@gmail.com>
2020-07-19 13:24:33 +0300

    Created a preview renderer for shared blocks

--- AlloyTraining/Controllers/TeaserBlockController.cs
+++ AlloyTraining/Controllers/TeaserBlockController.cs
@@ -1,11 +1,13 @@
 ﻿using AlloyTraining.Models.Blocks;
 using AlloyTraining.Models.ViewModels;
+using EPiServer.Framework.DataAnnotations;
 using EPiServer.Web.Mvc;
 using System;
 using System.Web.Mvc;
 
 namespace AlloyTraining.Controllers
 {
+    [TemplateDescriptor(Tags = new[] { SiteTags.Full }, AvailableWithoutTag = true)]
     public class TeaserBlockController : BlockController<TeaserBlock>
     {
         public override ActionResult Index(TeaserBlock currentBlock)
@@ -17,5 +19,15 @@ namespace AlloyTraining.Controllers
             };
             return PartialView(viewmodel);
         }
+    }
+
+    [TemplateDescriptor(Tags = new[] { SiteTags.Wide })]
+    public class TeaserBlockWideController : TeaserBlockController
+    {
+    }
+
+    [TemplateDescriptor(Tags = new[] { SiteTags.Narrow })]
+    public class TeaserBlockNarrowController : TeaserBlockController
+    {
     }
 }

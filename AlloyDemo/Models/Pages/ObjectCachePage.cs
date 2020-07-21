93f591e446bccecdde8b5e6c148f5e0819acebe4
Artur Vasyliev <princeartik@gmail.com>
2020-07-21 12:07:31 +0300

    Implemented object cache page

---
+++ AlloyDemo/Models/Pages/ObjectCachePage.cs
@@ -0,0 +1,13 @@
+﻿using EPiServer.DataAnnotations;
+
+namespace AlloyDemo.Models.Pages
+{
+    [SiteContentType(DisplayName = "Object Cache", 
+        GroupName = Global.GroupNames.Specialized,
+        Description = "View the contents of the object cache.")]
+    [SiteImageUrl]
+    [AvailableContentTypes(IncludeOn = new[] { typeof(StartPage) })]
+    public class ObjectCachePage : SitePageData
+    {
+    }
+}
